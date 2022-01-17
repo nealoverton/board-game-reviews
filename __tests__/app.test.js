969af3376d9c7702c5e5e0f7d1a5d901a93c286c
nealoverton <nealjoverton@gmail.com>
2022-01-17 18:21:55 +0200

    refactored using async await

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,6 +1,7 @@
 const db = require("../db/connection.js");
 const testData = require("../db/data/test-data/index.js");
-const seed = require("../db/seeds/seed.js");
+// const seed = require("../db/seeds/seed.js");
+const seed = require("../db/seeds/seed-with-await.js");
 
 beforeEach(() => seed(testData));
 afterAll(() => db.end());
--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -7,111 +7,96 @@ const {
   formatComments,
 } = require("./seed-formatting");
 
-const seed = (data) => {
+const seed = async (data) => {
   const { categoryData, commentData, reviewData, userData } = data;
-  // 1. create tables
-  return db
-    .query(`DROP TABLE IF EXISTS categories, users, reviews, comments;`)
-    .then(() => {
-      return db.query(
-        `CREATE TABLE categories(
-          slug VARCHAR(50) PRIMARY KEY,
-          description VARCHAR(100) NOT NULL
+
+  await db.query(`DROP TABLE IF EXISTS categories, users, reviews, comments;`);
+
+  await db.query(
+    `CREATE TABLE categories(
+          slug VARCHAR(255) PRIMARY KEY,
+          description VARCHAR(255) NOT NULL
         );`
-      );
-    })
-    .then(() => {
-      return db.query(
-        `CREATE TABLE users(
-          username VARCHAR(50) PRIMARY KEY,
+  );
+  await db.query(
+    `CREATE TABLE users(
+          username VARCHAR(255) PRIMARY KEY,
           avatar_url VARCHAR(2000) NOT NULL,
-          name VARCHAR(50) NOT NULL
+          name VARCHAR(255) NOT NULL
         );`
-      );
-    })
-    .then(() => {
-      return db.query(
-        `CREATE TABLE reviews(
+  );
+  await db.query(
+    `CREATE TABLE reviews(
           review_id SERIAL PRIMARY KEY,
-          title VARCHAR(2000) NOT NULL,
+          title VARCHAR(255) NOT NULL,
           review_body TEXT NOT NULL,
-          designer VARCHAR(100) NOT NULL,
+          designer VARCHAR(255) NOT NULL,
           review_img_url VARCHAR(2000) DEFAULT 'https://images.pexels.com/photos/163064/play-stone-network-networked-interactive-163064.jpeg',
           votes INT DEFAULT 0,
-          category VARCHAR(50) NOT NULL REFERENCES categories(slug),
-          owner VARCHAR(50) NOT NULL REFERENCES users(username),
+          category VARCHAR(255) NOT NULL REFERENCES categories(slug),
+          owner VARCHAR(255) NOT NULL REFERENCES users(username),
           created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
         );`
-      );
-    })
-    .then(() => {
-      return db.query(
-        `CREATE TABLE comments(
+  );
+
+  await db.query(
+    `CREATE TABLE comments(
           comment_id SERIAL PRIMARY KEY,
-          author VARCHAR(50) NOT NULL REFERENCES users(username),
+          author VARCHAR(255) NOT NULL REFERENCES users(username),
           review_id INT NOT NULL REFERENCES reviews(review_id),
           votes INT DEFAULT 0,
           created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
           body TEXT NOT NULL
         );`
-      );
-    })
-    .then(() => {
-      const formattedCategories = formatCategories(categoryData);
-      const sql = format(
-        `INSERT INTO categories
+  );
+
+  const formattedCategories = formatCategories(categoryData);
+  const categoriesSql = format(
+    `INSERT INTO categories
         (slug, description)
         VALUES %L
         RETURNING *
         ;`,
-        formattedCategories
-      );
+    formattedCategories
+  );
 
-      return db.query(sql);
-    })
-    .then(() => {
-      const formattedUsers = formatUsers(userData);
-      const sql = format(
-        `INSERT INTO users
+  await db.query(categoriesSql);
+
+  const formattedUsers = formatUsers(userData);
+  const usersSql = format(
+    `INSERT INTO users
         (username, avatar_url, name)
         VALUES %L
         RETURNING *
         ;`,
-        formattedUsers
-      );
+    formattedUsers
+  );
 
-      return db.query(sql);
-    })
-    .then(() => {
-      const formattedReviews = formatReviews(reviewData);
-      const sql = format(
-        `INSERT INTO reviews
+  await db.query(usersSql);
+
+  const formattedReviews = formatReviews(reviewData);
+  const reviewsSql = format(
+    `INSERT INTO reviews
         (title, review_body, designer, review_img_url, votes, category, owner, created_at)
         VALUES %L
         RETURNING *
         ;`,
-        formattedReviews
-      );
+    formattedReviews
+  );
 
-      return db.query(sql);
-    })
-    .then(() => {
-      const formattedComments = formatComments(commentData);
-      const sql = format(
-        `INSERT INTO comments
+  await db.query(reviewsSql);
+
+  const formattedComments = formatComments(commentData);
+  const commentsSql = format(
+    `INSERT INTO comments
         (author, review_id, votes, created_at, body)
         VALUES %L
         RETURNING *
         ;`,
-        formattedComments
-      );
+    formattedComments
+  );
 
-      return db.query(sql);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-  // 2. insert data
+  await db.query(commentsSql);
 };
 
 module.exports = seed;
