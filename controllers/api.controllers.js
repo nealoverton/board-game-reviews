bc9a4aa361dfb4fddc3e8d0ca4baef5d9c03f18f
nealoverton <nealjoverton@gmail.com>
2022-01-19 17:46:58 +0200

    addresses double stringification of endpoints json

--- controllers/api.controllers.js
+++ controllers/api.controllers.js
@@ -2,7 +2,8 @@ const fs = require("fs/promises");
 
 exports.getEndpoints = async (req, res, next) => {
   try {
-    const endpoints = await fs.readFile("./endpoints.json", "utf-8");
+    const endpointsString = await fs.readFile("./endpoints.json", "utf-8");
+    const endpoints = JSON.parse(endpointsString);
     res.status(200).send({ endpoints });
   } catch (err) {
     next(err);
